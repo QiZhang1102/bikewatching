f0409273d0a37f8fc00439dc9566217fb6962653
QiZhang1102 <zq20021102mia@gmail.com>
2025-11-14 14:47:21 -0800

    1

--- map.js
+++ map.js
@@ -14,3 +14,43 @@ const map = new mapboxgl.Map({
   minZoom: 5, // Minimum allowed zoom
   maxZoom: 18, // Maximum allowed zoom
 });
+
+
+map.on('load', async () => {
+
+  // Adding the Data Source with addSource
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+});
+
+  // Visualizing the data with addLayer
+  map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: {
+  'line-color': '#32D400',  // A bright green using hex code
+  'line-width': 5,          // Thicker lines
+  'line-opacity': 0.6       // Slightly less transparent
+},
+  });
+
+
+
+map.addSource('cambridge_route', {
+  type: 'geojson',
+  data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson'
+});
+
+map.addLayer({
+  id: 'cambridge-bike-lanes',
+  type: 'line',
+  source: 'cambridge_route',
+  paint: {
+    'line-color': '#32D400',
+    'line-width': 5,
+    'line-opacity': 0.6
+  }
+});
+});
